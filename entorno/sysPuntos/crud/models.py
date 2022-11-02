3ee4a4584a9e0faeb67d24899c1c47733f78609b
Yeison <ye.rivera@duocuc.cl>
2022-11-02 18:45:11 -0300

    Conexión a oracle, creación de modelos cliente y dirección

--- entorno/sysPuntos/crud/models.py
+++ entorno/sysPuntos/crud/models.py
@@ -1,3 +1,33 @@
+# This is an auto-generated Django model module.
+# You'll have to do the following manually to clean this up:
+#   * Rearrange models' order
+#   * Make sure each model has one field with primary_key=True
+#   * Make sure each ForeignKey and OneToOneField has `on_delete` set to the desired behavior
+#   * Remove `managed = False` lines if you wish to allow Django to create, modify, and delete the table
+# Feel free to rename the models, but don't rename db_table values or field names.
 from django.db import models
 
-# Create your models here.
+
+class Cliente(models.Model):
+    rut = models.CharField(primary_key=True, max_length=8)
+    dv = models.CharField(max_length=1)
+    username = models.CharField(max_length=50)
+    password = models.CharField(max_length=20)
+    nombre = models.CharField(max_length=30)
+    apellidopa = models.CharField(max_length=50)
+    apellidoma = models.CharField(max_length=50)
+    id_direccion = models.ForeignKey('Direccion', models.DO_NOTHING, db_column='id_direccion')
+
+    class Meta:
+        managed = False
+        db_table = 'cliente'
+
+
+class Direccion(models.Model):
+    id_direccion = models.AutoField(primary_key=True)
+    nombre = models.CharField(max_length=30, blank=True, null=True)
+    descripcion = models.CharField(max_length=100, blank=True, null=True)
+
+    class Meta:
+        managed = False
+        db_table = 'direccion'
--- entorno/sysPuntos/sysPuntos/settings.py
+++ entorno/sysPuntos/sysPuntos/settings.py
@@ -77,8 +77,15 @@ WSGI_APPLICATION = 'sysPuntos.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.oracle',
+        'NAME': '127.0.0.1:1521/xe',
+        'USER': 'c##arqDBF',
+        'PASSWORD': 'oracle1',
+        'TEST': {
+            'USER': 'default_test',
+            'TBLSPACE': 'default_test_tbls',
+            'TBLSPACE_TMP': 'default_test_tbls_tmp',
+        },
     }
 }
 
