3416bd0d22d1e902d0e1a9e1575efca5a8583feb
RichardRomero <ric.romero@duocuc.cl>
2022-11-16 01:31:51 -0300

    agregar al admin

--- entorno/sysPuntos/crud/admin.py
+++ entorno/sysPuntos/crud/admin.py
@@ -1,8 +1,13 @@
 from django.contrib import admin
-from .models import cliente, direccion, local, promocion
+from .models import cliente, direccion, local, promocion, trabajador, metodo_pago, producto, pedido, detalle_pedido
 
 # Register your models here.
 admin.site.register(cliente)
 admin.site.register(direccion)
 admin.site.register(local)
 admin.site.register(promocion)
+admin.site.register(metodo_pago)
+admin.site.register(trabajador)
+admin.site.register(producto)
+admin.site.register(pedido)
+admin.site.register(detalle_pedido)
