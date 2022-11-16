10375d51fe0a8f0a2b31e0d9293607b87046e2db
RichardRomero <ric.romero@duocuc.cl>
2022-11-16 01:00:58 -0300

    cambios en los modelos

--- entorno/sysPuntos/crud/models.py
+++ entorno/sysPuntos/crud/models.py
@@ -4,20 +4,20 @@ from django.contrib.auth.models import User
 # Create your models here.
  
 class cliente(models.Model):
-    rut_cliente = models.CharField(unique=True, max_length = 10, primary_key=True)
-    username = models.ForeignKey(User, on_delete = models.CASCADE, null=True, blank = True )
-    direccion_cli = models.CharField( max_length = 50)
+    rut_cliente = models.CharField(unique=True, max_length = 10, primary_key=True, verbose_name='Rut')
+    username = models.ForeignKey(User, on_delete = models.CASCADE, null=True, blank = True)
+    direccion_cli = models.CharField( max_length = 50, verbose_name='Direccion Principal')
     nombre = models.CharField(max_length = 30)
-    apellido_pa = models.CharField(max_length = 30)
-    apellido_ma = models.CharField(max_length = 30)
+    apellido_pa = models.CharField(max_length = 30, verbose_name='Apellido Paterno')
+    apellido_ma = models.CharField(max_length = 30, verbose_name='Apellido Materno')
     
     def __str__(self):
         return self.rut_cliente
     
 class direccion(models.Model):
-    nombre_dir = models.CharField(max_length = 50)
+    nombre_dir = models.CharField(max_length = 50, verbose_name='Nombre direcion')
     descripcion = models.TextField()
-    cliente_dir = models.ForeignKey(cliente, on_delete = models.CASCADE, null = True, blank = True)
+    cliente_dir = models.ForeignKey(cliente, on_delete = models.CASCADE, null = True, blank = True, verbose_name='Direccion cliente')
     def __str__(self):
         return self.nombre_dir
     
@@ -29,18 +29,18 @@ class usuarioPrueba(models.Model):
         return self.rut
     
 class local(models.Model):
-    nombre_local: models.CharField(max_length=50, unique=True)
-    direccion_local: models.CharField(max_length=200)
+    nombre_local = models.CharField(max_length=50, unique= True, null=True, verbose_name='nombre')
+    direccion_local = models.CharField(max_length=200, null=True, verbose_name='direccion')
     
     def __str__(self):
         return self.nombre_local
     
 class promocion(models.Model):
-    id_local: models.ForeignKey(local, on_delete=models.CASCADE)
-    descripcion_promo: models.CharField(max_length=50)
-    nombre_promo: models.CharField(max_length=50)
-    fecha_ini: models.DateField(auto_now_add=True)
-    fecha_fin: models.DateField(auto_now_add=False)
+    id_local = models.ForeignKey(local, on_delete=models.CASCADE)
+    descripcion_promo = models.CharField(max_length=50, null=True, verbose_name='Descripcion')
+    nombre_promo = models.CharField(max_length=50 , null=True, verbose_name='nombre')
+    fecha_ini = models.DateField(auto_now_add=False, null=True, verbose_name='fecha inicio')
+    fecha_fin = models.DateField(auto_now_add=False, null=True, verbose_name='fecha fin')
     
     def __str__(self):
         return self.nombre_promo
